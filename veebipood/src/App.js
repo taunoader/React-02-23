0e6540d847293f9ca4602bcaa55baccad6c32211
taunoader <taunoader@gmail.com>
2023-02-20 12:12:23 +0200

    Uus Reacti projekt + navigeerimine

---
+++ veebipood/src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+
+import { Link, Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Link to="/avaleht">
+          <img className='pilt' src="https://estonia.ee/wp-content/uploads/nobe_netist_4.jpg" alt="" />
+      </Link>
+
+       <Link to="/ostukorv">
+          <button className='nupp'>Nupp</button>
+       </Link>
+
+       <Link to="/lisa-toode">
+          <button className='nupp'>Toodet lisama</button>
+       </Link>
+
+
+<Routes>
+  <Route path="avaleht" element={<div>123</div>} />
+  <Route path="ostukorv" element={<div>0</div>} />
+  <Route path="lisa-toode" element={<div>LT</div>} />
+</Routes>
+    </div>
+  );
+}
+
+export default App;
