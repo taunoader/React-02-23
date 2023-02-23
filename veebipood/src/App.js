037ee694263fec0d2e039159db40578a0d583f0e
taunoader <taunoader@gmail.com>
2023-02-23 11:24:15 +0200

    pages+failide import, useState, dunaamiline valjakuvamine &&

--- veebipood/src/App.js
+++ veebipood/src/App.js
@@ -2,11 +2,14 @@
 import './App.css';
 
 import { Link, Route, Routes } from 'react-router-dom'
+import Avaleht from './pages/Avaleht';
+import Ostukorv from './pages/Ostukorv';
+import LisaToode from './pages/LisaToode';
 
 function App() {
   return (
     <div className="App">
-      <Link to="/avaleht">
+      <Link to="/">
           <img className='pilt' src="https://estonia.ee/wp-content/uploads/nobe_netist_4.jpg" alt="" />
       </Link>
 
@@ -18,11 +21,10 @@ function App() {
           <button className='nupp'>Toodet lisama</button>
        </Link>
 
-
     <Routes>
-       <Route path="avaleht" element={<div>123</div>} />
-       <Route path="ostukorv" element={<div>0</div>} />
-        <Route path="lisa-toode" element={<div>LT</div>} />
+       <Route path="" element={ <Avaleht/> } />
+       <Route path="ostukorv" element={ <Ostukorv /> } />
+        <Route path="lisa-toode" element={ <LisaToode /> } />
    </Routes>
   </div>
   );
